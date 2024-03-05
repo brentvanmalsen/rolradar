ec669608896e096dcbbad2b284999d70205cb209
JorgRijdt <jorgvanderijdt5@gmail.com>
2024-03-05 14:28:54 +0100

    profiel sectie toegevoegd

--- lib/ontdek_page.dart
+++ lib/ontdek_page.dart
@@ -3,10 +3,192 @@ import 'package:flutter/material.dart';
 class OntdekPage extends StatelessWidget {
   const OntdekPage({Key? key}) : super(key: key);
 
+  void _startSearch(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Zoeken'),
+          content: TextField(
+            decoration: InputDecoration(
+              hintText: 'Voer zoekterm in',
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Annuleren'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Voer hier de logica uit voor de zoekactie
+                Navigator.of(context).pop();
+              },
+              child: Text('Zoeken'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: const Text('kankerkekekekrkrkr'),
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: AppBar(
+          title: TextField(
+            decoration: InputDecoration(
+              hintText: 'Zoek naar namen, rollen en of vaardigheden ',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+                borderSide: BorderSide.none,
+              ),
+              filled: true,
+              fillColor: Colors.grey[200],
+              prefixIcon: IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  _startSearch(context);
+                },
+              ),
+            ),
+          ),
+          automaticallyImplyLeading: false,
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: Container(
+                  height: 30,
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Naam',
+                      style:
+                          TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 30,
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Rol',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 30,
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Functie',
+                      style:
+                          TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+              height: 20), // Ruimte onder de knoppen en boven de profiel sectie
+          // Profiel sectie
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Row(
+              children: [
+                // Linker foto
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.grey[300],
+                  ),
+                  // Placeholder voor de linker foto
+                  child: Icon(Icons.person),
+                ),
+                SizedBox(width: 10),
+                // Tekstgedeelte
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Naam',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        'Rol',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                        ),
+                      ),
+                      Text(
+                        'Functie',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 10),
+                // Rechter foto
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.grey[300],
+                  ),
+                  // Placeholder voor de rechter foto
+                  child: Icon(Icons.person),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

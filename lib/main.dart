78f8b2418443fa9ee0295ef8f6ad00ac02a1f8cd
Brent van Malsen <i507960@student.fontys.nl>
2024-03-06 14:30:13 +0100

    @ knop

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'ontdek_page.dart'; // Importeer OntdekPage class
-import 'profiel_page.dart'; // Importeer ProfielPage class
+import 'ontdek_page.dart';
+import 'profiel_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'RolRadar',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme:
             ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 0, 0, 0)),
@@ -35,8 +36,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final List<Widget> _pages = [
     PageOne(),
-    OntdekPage(), // Gebruik OntdekPage
-    ProfielPage(), // Gebruik ProfielPage
+    OntdekPage(),
+    ProfielPage(),
   ];
 
   void _onItemTapped(int index) {
@@ -80,41 +81,57 @@ class PageOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      // Plaatst de inhoud van de widget in het midden van het scherm
       child: Column(
-        // Een widget die zijn kinderen verticaal onder elkaar plaatst
-        mainAxisAlignment:
-            MainAxisAlignment.center, // Centreert de kolom in de verticale as
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          // Lijst van widgets die verticaal onder elkaar worden geplaatst
-          const SizedBox(height: 20), // Een lege ruimte van 20 pixels hoogte
+          const SizedBox(height: 20),
           Container(
-            // Een rechthoekige doos voor meer lay-out
-            padding: const EdgeInsets.all(
-                16), // Vulling van 16 pixels aan alle kanten van de doos
+            padding: const EdgeInsets.all(16),
             decoration: BoxDecoration(
-              // Decoratie voor de doos, zoals achtergrondkleur en rand
-              color: Colors.grey[200], // Achtergrondkleur lichtgrijs
-              borderRadius:
-                  BorderRadius.circular(10), // Afgeronde hoeken van 10 pixels
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(10),
             ),
             child: Column(
-              // Een andere kolom van widgets binnen de doos
               children: [
                 const Text(
-                  // Tekstwidget met de tekst binnen de doos
-                  'Dit is een tekst in de box!', // Tekst binnen de doos
-                  style: TextStyle(fontSize: 16), // Lettergrootte van 16 pixels
+                  'Belbin-rollen test',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-                const SizedBox(
-                    height: 10), // Een lege ruimte van 10 pixels hoogte
+                const SizedBox(height: 10),
+                const Text(
+                  'Ontdek jouw unieke teamstijl met de Belbin-rollen test.',
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 10),
                 ElevatedButton(
-                  // Een verhoogde knopwidget
-                  onPressed: () {
-                    // Functie voor wat er moet gebeuren wanneer de knop wordt ingedrukt
-                  },
-                  child:
-                      const Text('Klik hier'), // Tekst op de knop: 'Klik hier'
+                  onPressed: () {},
+                  child: const Text('Klik hier'),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.blue[200],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              children: [
+                const Text(
+                  'Belbin-rollen test',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Ontdek jouw unieke teamstijl met de Belbin-rollen test.',
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Klik hier ook!'),
                 ),
               ],
             ),

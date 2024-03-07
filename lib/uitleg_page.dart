201fa9b1d4751badf24358bab8be34ee26058803
Brent van Malsen <i507960@student.fontys.nl>
2024-03-07 11:42:49 +0100

    Uitleg aan de Uitleg pagina toegevoegd

--- lib/uitleg_page.dart
+++ lib/uitleg_page.dart
@@ -1,21 +1,107 @@
 import 'package:flutter/material.dart';
 
-class UitlegPage extends StatelessWidget {
+class UitlegPage extends StatefulWidget {
   const UitlegPage({Key? key}) : super(key: key);
+
+  @override
+  State<UitlegPage> createState() => _UitlegPageState();
+}
+
+class _UitlegPageState extends State<UitlegPage> {
+  String? selectedFunction;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Uitleg Pagina'),
+        backgroundColor: Color(0xFF1560BD),
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
       ),
-      body: Center(
-        child: const Text('Dit is de uitleg pagina!'),
+      body: Container(
+        color: const Color(0xFF1560BD),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Uitleg',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'De Belbin-rol analyse test is een korte en effectieve manier om jouw teamrol binnen een team te ontdekken. Door middel van scenario-vragen word je uitgedaagd om te reageren op verschillende situaties die zich in een team kunnen voordoen. De test duurt ongeveer 15 minuten en biedt inzicht in jouw natuurlijke neigingen en sterke punten binnen een teamomgeving.',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'RobotoLight',
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    DropdownButtonFormField<String>(
+                      decoration: InputDecoration(
+                        labelText: 'Functie',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      value: selectedFunction,
+                      onChanged: (newValue) {
+                        setState(() {
+                          selectedFunction = newValue!;
+                        });
+                      },
+                      items: <String>[
+                        'Functie 1',
+                        'Functie 2',
+                        'Functie 3',
+                        'Functie 4',
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Voeg hier de navigatielogica toe
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromRGBO(222, 119, 34, 1),
+                        foregroundColor: Color.fromARGB(255, 250, 250, 249),
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        textStyle: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      child: const Text('Start de test'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0, // Index van de huidige pagina, verander indien nodig
+        currentIndex: 0,
         onTap: (index) {
-          // Navigatie naar andere pagina's
           // Voeg hier eventuele navigatielogica toe
         },
         items: const <BottomNavigationBarItem>[

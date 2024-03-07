8c55d36d4d3b0908e4b26bc4d18bf5da23b87ab2
JorgRijdt <jorgvanderijdt5@gmail.com>
2024-03-07 09:27:09 +0100

    Eerste opzet profielpagina gemaakt met de layout van het design

--- lib/profiel_page.dart
+++ lib/profiel_page.dart
@@ -5,8 +5,58 @@ class ProfielPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: const Text('Profiel'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profiel'),
+      ),
+      backgroundColor: Color(0xFFDE7722),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/images/profile_pic.jpg'),
+            ),
+            SizedBox(height: 20),
+            Text('Voornaam Achternaam', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 10),
+            Text('E-mailadres', style: TextStyle(fontSize: 16)),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Actie voor de eerste knop
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFFDE7722), // achtergrondkleur
+              ),
+              child: Text(
+                'Bekijk mijn team',
+                style: TextStyle(
+                  color: Colors.white, // tekstkleur
+                  fontWeight: FontWeight.bold, // vetgedrukt
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                // Actie voor de tweede knop
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFFDE7722), // achtergrondkleur
+              ),
+              child: Text(
+                'Bekijk test geschiedenis',
+                style: TextStyle(
+                  color: Colors.white, // tekstkleur
+                  fontWeight: FontWeight.bold, // vetgedrukt
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
